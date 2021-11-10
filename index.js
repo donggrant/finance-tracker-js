8f772271bbc363f481f32c78226c2df380c88242
Grant Dong <gdd7jh@virginia.edu>
2021-11-10 16:15:29 -0500

    Changes

--- index.js
+++ index.js
@@ -33,6 +33,29 @@ function load_transactions() {
     // update balance with all amounts in transactions
 }
 
+const form  = document.getElementById('new_transaction');
+form.addEventListener('submit', (event) => {
+    var name = form.elements["name"];
+    var date = form.elements["purchase_date"];
+    var amount = form.elements["amount"];
+    if (form.elements["type"] === "debit"){
+        amount *= -1;
+    }
+    var transaction = new Transaction(name, date, amount);
+    transaction.push(transaction);
+    window.location.reload();
+});
+
+function clear_transactions() {
+    transactions = [];
+    window.location.reload();
+}
+
+function delete_transaction(index) {
+    transactions.splice(index, 1);
+    window.location.reload();
+}
+
 
 function update_balance(amount) {
     balance += amount;
