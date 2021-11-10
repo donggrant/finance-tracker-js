01e7f2083084242cf20e72a2e8e69ce78e3f8b14
Hunter Vaccaro <hv@Hunters-MacBook-Pro.local>
2021-11-10 17:23:23 -0500

    refactored display transactions

--- index.js
+++ index.js
@@ -40,42 +40,32 @@ function display_transactions() {
     var body = document.getElementById("transaction_body");
     for(var i = 0; i < transactions.length; i++) {
         var transaction = transactions[i];
+        var row = document.createElement("tr");    
+        for(const properties in transaction) {
+            var property = document.createElement("td");
+            var property_text = document.createTextNode(transaction[properties]);
 
-        var row = document.createElement("tr");
-    
-        var name = document.createElement("td");
-        var name_text = document.createTextNode(transaction.name);
-
-        var purchase_date = document.createElement("td");
-        var purchase_date_text = document.createTextNode(transaction.purchase_date);
-
-        var amount = document.createElement("td");
-        var amount_text = document.createTextNode(transaction.amount);
-
-        var amount = document.createElement("td");
-        var amount_text = document.createTextNode(transaction.amount);
+            property.appendChild(property_text);
+            row.appendChild(property);
+        }
 
         var del = document.createElement("td");
-        var delete_button = document.createElement("button");
-        delete_button.className = "btn btn-danger";
-        delete_button.value = "delete";
-        delete_button.onclick = (function(index) {return function() {delete_transaction(index);}})(i);
-
-        name.appendChild(name_text);
-        row.appendChild(name);
-
-        purchase_date.appendChild(purchase_date_text);
-        row.appendChild(purchase_date);
-        
-        amount.appendChild(amount_text);
-        row.appendChild(amount);
-
-        del.appendChild(delete_button);
-        row.appendChild(del);
+        del.appendChild(delete_button(i));
+        row.appendChild(del); 
 
         body.appendChild(row);
     }
 }
+
+function delete_button(i) {
+    var delete_button = document.createElement("button");
+    delete_button.className = "btn btn-danger";
+    delete_button.value = "delete";
+    delete_button.onclick = (function(index) {return function() {delete_transaction(index);}})(i);
+
+    return delete_button;
+}
+
 
 const form  = document.getElementById('new_transaction');
 form.addEventListener('submit', (event) => {
